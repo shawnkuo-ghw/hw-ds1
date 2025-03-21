169c10e546057fbf281bda5891c939fcbea666b3
ShawnKuo <shawnkuo2019@163.com>
2025-03-21 20:04:08 +0800

    assignment A1 really start

---
+++ assignment_A1/ex_04_Galactic_Siege_The_Last_Line_of_Defense_40pts/ghw/code/src/test/java/hw1/WarriorTest.java
@@ -0,0 +1,9 @@
+package hw1;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class WarriorTest {
+    // To Do
+}
